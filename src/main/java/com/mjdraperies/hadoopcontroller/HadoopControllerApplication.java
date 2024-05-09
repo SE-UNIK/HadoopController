785e9d923f89590d1705fd1f874529e06204211d
John Song <jstarts86@gmail.com>
2024-05-10 00:33:02 +0900

    first commit

---
+++ src/main/java/com/mjdraperies/hadoopcontroller/HadoopControllerApplication.java
@@ -0,0 +1,12 @@
+package com.mjdraperies.hadoopcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopControllerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mjdraperies/hadoopcontroller/HadoopControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjdraperies.hadoopcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
