b92310606c9b080e732b8773e8525fba3fe32bbc
Aza Bibo <biboaza37@gmail.com>
2024-05-21 14:00:52 +0900

    FileModel.java

--- src/main/java/com/unik/hadoopcontroller/model/FileModel.java
+++ src/main/java/com/unik/hadoopcontroller/model/FileModel.java
@@ -1,4 +1,51 @@
 package com.unik.hadoopcontroller.model;
 
-public class FileModel {
+import java.io.Serializable;
+
+public class FileModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String fileName;
+    private long fileSize;
+    private String fileType;
+    private String filePath;
+
+    public FileModel(String fileName, long fileSize, String fileType, String filePath) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.fileType = fileType;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
 }
