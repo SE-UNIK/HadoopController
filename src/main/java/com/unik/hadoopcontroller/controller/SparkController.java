fca05cbdbca510e6ce5ef9b707e1ac10e35dab90
John Song <jstarts86@gmail.com>
2024-06-06 19:26:15 +0900

    Spark SubmitJOb refining

--- src/main/java/com/unik/hadoopcontroller/controller/SparkController.java
+++ src/main/java/com/unik/hadoopcontroller/controller/SparkController.java
@@ -9,33 +9,18 @@ import java.io.File;
 
 @RestController
 @RequestMapping("/spark")
-@CrossOrigin(origins = "http://localhost:8081")
 public class SparkController {
 
     @Autowired
-    private SparkSubmitJobService sparkJobService;
+    private SparkSubmitJobService sparkSubmitJobService;
 
     @PostMapping("/submit")
-    public String launchSparkJob(@RequestBody SparkModel sparkJobModel, @RequestParam String fileName, @RequestParam String hdfsFilePath) { // change file type
-        try {
-            File output = sparkJobService.launchSparkJob(sparkJobModel, fileName);
-            return "Spark Job completed";
-        } catch (Exception e) {
-            return "Error doing job submission: " + e.getMessage();
+    public String launchSparkJob(@RequestBody SparkModel sparkModel, @RequestParam String fileName, @RequestParam String hdfsFilePath) {
+        File outputFile = sparkSubmitJobService.launchSparkJob(sparkModel, fileName);
+        if (outputFile != null) {
+            return "Spark job submitted successfully. Output file: " + outputFile.getPath();
+        } else {
+            return "Failed to submit Spark job.";
         }
     }
-
-
-
-    // @RequestMapping("read-csv")
-    // public ResponseEntity<String> getRowCount() {
-    //     Dataset<Row> dataset = sparkSession.read().option("header", "true").csv("../spark-spring-boot/src/main/resources/raw_data.csv");
-    //     String html = String.format("<h1>%s</h1>", "Running Apache Spark on/with support of Spring boot") +
-    //             String.format("<h2>%s</h2>", "Spark version = "+sparkSession.sparkContext().version()) +
-    //             String.format("<h3>%s</h3>", "Read csv..") +
-    //             String.format("<h4>Total records %d</h4>", dataset.count()) +
-    //             String.format("<h5>Schema <br/> %s</h5> <br/> Sample data - <br/>", dataset.schema().treeString()) +
-    //             dataset.showString(20, 20, true);
-    //     return ResponseEntity.ok(html);
-    // }
 }
