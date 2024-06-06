249d1532c459a9a0e7922c1b8e3a82b06d7339d3
John Song <jstarts86@gmail.com>
2024-06-06 19:16:32 +0900

    Spark SubmitJOb refining

--- src/main/java/com/unik/hadoopcontroller/service/SparkSubmitJobService.java
+++ src/main/java/com/unik/hadoopcontroller/service/SparkSubmitJobService.java
@@ -2,74 +2,66 @@ package com.unik.hadoopcontroller.service;
 
 import com.unik.hadoopcontroller.CustomMultipartFile;
 import com.unik.hadoopcontroller.model.SparkModel;
+import org.apache.spark.launcher.SparkLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.apache.spark.launcher.SparkLauncher;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-/*
-* using SparkLauncher instead of SparkSubmit as SparkLauncher is a process so we can track its stdout
-* only need to provide PATHs of input file(s) [from hdfs] and pyspark algorithm(s) [from spark dir], and output file [to hdfs]
-* launch spark -> redirect output results to be saved in a file then stored in hdfs            OR change type to hdfsfilemodel and use post method to store to hdfs??
-* -->> change output path to set to a new file create right before redirect method and pass new file path for getting output
-* */
-
-
 @Service
 public class SparkSubmitJobService {
-    //    private final SparkConf sparkConf;
-//    private final SparkModel sparkModel;
+
     private final String systemHadoopRootDir = "/home/hadoop";
     private final String systemSparkAlgorithmsDir = systemHadoopRootDir + "/spark/algorithms/";
     private final String hdfsRootDir = "/user/hadoop/";
+
     @Autowired
-    HdfsDirectService hdfsDirectService;
-	@Autowired
-	private SparkModel sparkModel;
-    //String wordCountScript = sparkAlgorithmsDir + "wordcount.py";
-    //String destination = "/user/hadoop/";
+    private HdfsDirectService hdfsDirectService;
+
+    @Autowired
+    private SparkModel sparkModel;
 
     private void redirectOutput(InputStream inputStream, String filePath) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath));
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = inputStream.read(buffer)) != -1) {
-            fileOutputStream.write(buffer, 0, length);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath))) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, length);
+            }
         }
-        fileOutputStream.close();
     }
 
     public File launchSparkJob(SparkModel sparkJobModel, String fileName) {
         try {
-            String inputFilePath =
-                    sparkJobModel.getInputDirectoryPath() + sparkJobModel.getInputFileName();
+            String inputFilePath = hdfsRootDir + sparkJobModel.getInputDirectoryPath() + sparkJobModel.getInputFileName();
             Process spark = new SparkLauncher()
                     .setSparkHome(systemHadoopRootDir + "/spark")
                     .setAppResource(systemSparkAlgorithmsDir + sparkJobModel.getAlgorithmName())
                     .setMaster("yarn")
                     .setDeployMode("cluster")
-                    .addAppArgs(inputFilePath)   // should receive parquet file
+                    .addAppArgs(inputFilePath)
                     .setVerbose(true)
                     .launch();
 
-            // create new file here to pass to redirectOutput path
             String analysisFileName = "analysis_results_" + fileName;
             String title = fileName + "'s Analysis Results";
-            String[] author = {"Sparky"};
-            List<String> authors = Arrays.asList(author);
+            List<String> authors = Arrays.asList("Sparky");
             String systemOutputFilePath = "output/" + analysisFileName;
 
             File outputFile = new File(systemOutputFilePath);
+            if (!Files.exists(Paths.get("output"))) {
+                Files.createDirectories(Paths.get("output"));
+            }
 
             redirectOutput(spark.getInputStream(), systemOutputFilePath);
-            // redirectOutput(spark.getErrorStream(), sparkJobModel.getOutputPath());
 
             int exitCode = spark.waitFor();
             MultipartFile results = new CustomMultipartFile(outputFile);
@@ -83,5 +75,4 @@ public class SparkSubmitJobService {
             return null;
         }
     }
-
 }
